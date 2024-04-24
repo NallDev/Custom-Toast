ec9e8d76352d519ba54fd2e85bb1332e1cb55a4b
AfrinaldiLibrary <afrinaldi190401@gmail.com>
2024-04-24 15:06:31 +0700

    second commit

--- app/src/main/java/com/nalldev/customtoast/MainActivity.kt
+++ app/src/main/java/com/nalldev/customtoast/MainActivity.kt
@@ -5,16 +5,26 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.lifecycleScope
+import com.nalldev.customtoast.databinding.ActivityMainBinding
+import com.nalldev.naltoast.view.NalToast
 
 class MainActivity : AppCompatActivity() {
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        binding.tvHello.setOnClickListener {
+            NalToast.show(binding.root, "OK", lifecycleScope)
         }
     }
 }
